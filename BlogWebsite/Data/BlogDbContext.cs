5b173a78cd561661dccc4000946c9d65a73a2e28
iasaduzzaman <zasad.cse@gmail.com>
2025-11-20 17:23:46 +0600

    add nuget package

---
+++ BlogWebsite/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogWebsite.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebsite.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions option) : base(option)
+        {
+
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ BlogWebsite/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogWebsite.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+        public string DisplayName { get; set; }
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
