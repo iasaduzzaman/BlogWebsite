96fb4ad4faf716aecef241cf6d5a9165bede2722
iasaduzzaman <zasad.cse@gmail.com>
2025-11-30 01:52:43 +0600

    Update UI Design

---
+++ BlogWebsite/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebsite.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
