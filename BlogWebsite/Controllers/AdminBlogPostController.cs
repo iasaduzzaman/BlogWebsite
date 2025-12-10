a3f2e291cb44933ed9bbce24e87d7ab118f9c9cd
iasaduzzaman <zasad.cse@gmail.com>
2025-12-10 19:25:12 +0600

    Create BlogPost Controller

---
+++ BlogWebsite/Controllers/AdminBlogPostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebsite.Controllers
+{
+    public class AdminBlogPostController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
