4bd76279aec53b4037a4043e4123b5b9efad57f0
iasaduzzaman <zasad.cse@gmail.com>
2026-05-19 23:17:34 +0600

    Implemented tag support and refactor Home view model

--- BlogWebsite/Controllers/HomeController.cs
+++ BlogWebsite/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using BlogWebsite.Interfaces;
 using BlogWebsite.Models;
+using BlogWebsite.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogWebsite.Controllers
@@ -9,22 +10,31 @@ namespace BlogWebsite.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ITagRepository tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger, 
+            IBlogPostRepository blogPostRepository,
+            ITagRepository tagRepository )
         {
             _logger = logger;
             this.blogPostRepository = blogPostRepository;
+            this.tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
-            //getting all blogpost
+            //getting all blogs
             var BlogPost = await blogPostRepository.GetAllAsync();
              
             //getting all tags
+            var TagPost = await tagRepository.GetAllAsync();
 
+            var model = new HomeViewModel { 
+             BlogPosts = BlogPost,
+             Tags = TagPost
+            };
 
-            return View(BlogPost);
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ BlogWebsite/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BlogWebsite.Models.Domain;
+
+namespace BlogWebsite.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }  
+    }
+}
